aae976dcbb2e4391bfe2927694820b1f2e18d73c
秦佳锐 <qinjiarui@qinjiaruideMacBook-Pro.local>
2018-02-10 16:25:04 +0800

    run.m

---
+++ run.m
@@ -0,0 +1,4 @@
+survival_data_pre 'deep-bid-lands-data/2259/' 'train.emb' 'test.emb.z'
+MTLSA 'deep-bid-lands-data/2259/' 'train.emb' 'test.emb.z' 100 0.01  'z'
+survival_data_pre 'deep-bid-lands-data/2259/' 'train.emb' 'test.emb.z1'
+MTLSA 'deep-bid-lands-data/2259/' 'train.emb' 'test.emb.z1' 100 0.01  'z1'
